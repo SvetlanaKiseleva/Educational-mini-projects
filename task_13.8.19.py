43940bced2f8d654d6af75b9cb8aae65897b1dcc
SvetlanaKiseleva <svetlana_nhe@mail.ru>
2022-04-23 17:27:48 +0400

    A new file is added

--- task_13.8.19.py
+++ task_13.8.19.py
@@ -1,22 +1,20 @@
 t = int(input("Введите количество билетов: "))
 tickets_price = 0
-user_age = int(input("Введите ваш возраст: "))
-for tickets_price in range(t, t + 1):
+
+for i in range(t):
+    user_age = int(input("Введите возраст посетителя: "))
+
     if user_age < 18:
         print("Проход на конференцию бесплатный.")
     elif 18 <= user_age < 25:
-        print("Стоимость одного билета составит 990 рублей.")
-        if t > 3:
-            tickets_price = t * 990 * 0.9
-            print("Сумма к оплате со скидкой 10% (от 4-х человек): ", tickets_price, "рублей")
-        else:
-            tickets_price = t * 990
-            print("Сумма к оплате: ", tickets_price, "рублей")
+        tickets_price += 990
+        print("Стоимость билета составит 990 рублей.")
     elif user_age >= 25:
-        print("Стоимость одного билета составит 1390 рублей.")
-        if t > 3:
-            tickets_price = t * 1390 * 0.9
-            print("Сумма к оплате со скидкой 10% (от 4-х человек): ", tickets_price, "рублей")
-        else:
-            tickets_price = t * 1390
-            print("Сумма к оплате: ", tickets_price, "рублей")
+        tickets_price += 1390
+        print("Стоимость билета составит 1390 рублей.")
+
+if t > 3:
+    tickets_price = tickets_price * 0.9
+    print("Сумма к оплате со скидкой 10% (от 4-х человек): ", tickets_price, "рублей")
+else:
+    print("Сумма к оплате: ", tickets_price, "рублей")
